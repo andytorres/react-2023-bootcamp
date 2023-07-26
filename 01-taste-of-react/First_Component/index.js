7fc05f7bb4c95ddfbd2388eb732490c996227a43
Andy Torres Utra <andytorres.cu@gmail.com>
2023-07-26 19:05:45 -0400

    [ADD] Chapter 1

---
+++ 01-taste-of-react/First_Component/index.js
@@ -0,0 +1,11 @@
+
+class Hello extends React.Component {
+    state = {  } 
+    render() { 
+        return (<h1>Hello There</h1>);
+    }
+}
+ 
+// export default Hello;
+
+ReactDOM.render(<Hello/>, document.getElementById('root'));
