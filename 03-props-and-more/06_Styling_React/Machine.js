7aa242ce11c3f783e3adb5e7797109671b538415
Andy Torres Utra <andytorres.cu@gmail.com>
2023-07-26 20:19:54 -0400

    [IMP] Changing to local assets

--- 03-props-and-more/06_Styling_React/Machine.js
+++ 03-props-and-more/06_Styling_React/Machine.js
@@ -2,11 +2,11 @@ class Machine extends React.Component {
 	render() {
 		const { s1, s2, s3 } = this.props;
 		const winner = s1 === s2 && s2 === s3;
-		const colors = { fontSize: '50px', backgroundColor: 'purple' };
+		const styles = { fontSize: '50px', backgroundColor: 'purple' };
 
 		return (
 			<div className="Machine">
-				<p style={colors}>
+				<p style={styles}>
 					{s1} {s2} {s3}
 				</p>
 				<p className={winner ? 'win' : 'lose'}>{winner ? 'Winner!' : 'Loser!'}</p>
